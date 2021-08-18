d9d2ece92672cce0680128388ce3010569640f16
jeremiasacosta <jjere18@gmail.com>
2021-08-18 11:56:43 -0300

    Entrega1_2

--- js/products.js
+++ js/products.js
@@ -1,15 +1,9 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-//document.addEventListener("DOMContentLoaded", function (e) {
-
-//});
-
 
 function mostrarListaProductos(lista) {
-    let contenidoHtml = "";
+    let htmlContentToAppend = "";
     for (let product of lista) {
-    contenidoHtml += `
+    htmlContentToAppend += //agrego las propiedades de los productos.
+    `
     <a href="category-info.html" class="list-group-item list-group-item-action">
         <div class="row">
             <div class="col-3">
@@ -18,7 +12,7 @@ function mostrarListaProductos(lista) {
             <div class="col">
                 <div class="d-flex w-100 justify-content-between"> 
                     <h3 class="mb-1">` + product.name + `</h3>
-                    <small class="text-muted">` + product.soldCount + ` artículos</small>
+                    <small class="text-muted">` + product.soldCount + ` artículos vendidos</small>
                 </div>
                 <p class="mb-1">` +  product.cost + ` ` + product.currency + `</p>
                 <p class="mb-1">Detalle: ` + product.description + `</p>
@@ -27,19 +21,18 @@ function mostrarListaProductos(lista) {
     </a>
     `
     }
-   document.getElementById("containerProductos").innerHTML = contenidoHtml
+    //para poder mostrarlo en el html, agrego el contenido html al <div id="containerProductos">.
+   document.getElementById("containerProductos").innerHTML = htmlContentToAppend
 }
 
-
+//funcion para llamar la url que contiene los productos en formato JSON:
 function getJSONData(PRODUCTS_URL){
     var result = {};
     return fetch(PRODUCTS_URL)
       .then(repuesta => {
         if (repuesta.ok) {
           return repuesta.json();
-        } else {
-          throw Error(repuesta.statusText);
-        }
+        } else {throw Error(repuesta.statusText);}
       })
       .then(function (repuesta) {
         result.status = 'ok';
@@ -53,12 +46,13 @@ function getJSONData(PRODUCTS_URL){
       });
     }
 
-    document.addEventListener("DOMContentLoaded", function(e){
-        getJSONData(PRODUCTS_URL).then(function(resultObj){
-            if (resultObj.status === "ok")
-            {
-                //lista = resultObj.data;
-                mostrarListaProductos(resultObj.data);
-            }
-        });
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            mostrarListaProductos(resultObj.data);
+        }
     });
+});
