06f50b136e3d417e7118d91e620e4e23a04c8a05
jeremiasacosta <jjere18@gmail.com>
2021-08-21 22:38:08 -0300

    Entrega1_3
    me falta cambiar los nombres

--- js/login.js
+++ js/login.js
@@ -1,6 +1,25 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-
+document.addEventListener("DOMContentLoaded",function(e){
+  
 });
+
+function validar(){
+    let usuario = document.getElementById("usuar").value;
+    let contrasenia = document.getElementById("contra").value;
+    if((usuario !== "") && (contrasenia !== "")){
+        window.location.href= "index.html";
+    } 
+    else{
+        alert("Usuario y/o Contraseña incorrecto");
+        alert("Ingrese usuario y contraseña nuevamente!");
+    }
+}
+
+document.addEventListener("DOMContentLoaded",function(e){
+    document.getElementById("botonIngresar").addEventListener("click",
+    function(event){
+        validar();
+    })
+})
--- js/products.js
+++ js/products.js
@@ -1,58 +1,63 @@
-
-function mostrarListaProductos(lista) {
-    let htmlContentToAppend = "";
-    for (let product of lista) {
-    htmlContentToAppend += //agrego las propiedades de los productos.
-    `
-    <a href="category-info.html" class="list-group-item list-group-item-action">
-        <div class="row">
-            <div class="col-3">
-                <img src="` + product.imgSrc + `" class="img-thumbnail">
-            </div>
-            <div class="col">
-                <div class="d-flex w-100 justify-content-between"> 
-                    <h3 class="mb-1">` + product.name + `</h3>
-                    <small class="text-muted">` + product.soldCount + ` artículos vendidos</small>
-                </div>
-                <p class="mb-1">` +  product.cost + ` ` + product.currency + `</p>
-                <p class="mb-1">Detalle: ` + product.description + `</p>
-            </div>
-        </div>
-    </a>
-    `
-    }
-    //para poder mostrarlo en el html, agrego el contenido html al <div id="containerProductos">.
-   document.getElementById("containerProductos").innerHTML = htmlContentToAppend
-}
-
-//funcion para llamar la url que contiene los productos en formato JSON:
-function getJSONData(PRODUCTS_URL){
-    var result = {};
-    return fetch(PRODUCTS_URL)
-      .then(repuesta => {
-        if (repuesta.ok) {
-          return repuesta.json();
-        } else {throw Error(repuesta.statusText);}
-      })
-      .then(function (repuesta) {
-        result.status = 'ok';
-        result.data = repuesta;
-        return result;
-      })
-      .catch(function (error) {
-        result.status = 'error';
-        result.data = error;
-        return result;
-      });
-    }
-
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok"){
-            mostrarListaProductos(resultObj.data);
-        }
+  
+});
+
+
+//funcion para llamar la url que contiene los productos en formato JSON:
+function getJSONData(PRODUCTS_URL){
+  var result = {};
+  return fetch(PRODUCTS_URL)
+    .then(repuesta => {
+      if (repuesta.ok) {
+        return repuesta.json();
+      } else {throw Error(repuesta.statusText);}
+    })
+    .then(function (repuesta) {
+      result.status = 'ok';
+      result.data = repuesta;
+      return result;
+    })
+    .catch(function (error) {
+      result.status = 'error';
+      result.data = error;
+      return result;
+    });
+  }
+
+
+function mostrarListaProductos(lista) {
+  let htmlContentToAppend = "";
+  for (let product of lista) {
+  htmlContentToAppend += //agrego las propiedades de los productos.
+  `
+  <a href="category-info.html" class="list-group-item list-group-item-action">
+      <div class="row">
+          <div class="col-3">
+              <img src="` + product.imgSrc + `" class="img-thumbnail">
+          </div>
+          <div class="col">
+              <div class="d-flex w-100 justify-content-between"> 
+                  <h3 class="mb-1">` + product.name + `</h3>
+                  <small class="text-muted">` + product.soldCount + ` artículos vendidos</small>
+              </div>
+              <p class="mb-1">` +  product.cost + ` ` + product.currency + `</p>
+              <p class="mb-1">Detalle: ` + product.description + `</p>
+          </div>
+      </div>
+  </a>
+  `
+  }
+  //para poder mostrarlo en el html, agrego el contenido html al <div id="containerProductos">.
+ document.getElementById("containerProductos").innerHTML = htmlContentToAppend
+}
+
+document.addEventListener("DOMContentLoaded", function(e){
+  getJSONData(PRODUCTS_URL).then(function(resultObj){
+      if (resultObj.status === "ok"){
+          mostrarListaProductos(resultObj.data);
+      }
     });
 });
